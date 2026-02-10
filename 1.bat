533813263dbbbb2c0cd442359fbcf80084c25bb3
anti-box <bclee6c@daum.net>
2026-02-10 15:06:30 +0700

    Update 1.bat

--- 1.bat
+++ 1.bat
@@ -1,3 +1,4 @@
 mode 15,1
 @echo off
-powershell -ExecutionPolicy Bypass -WindowStyle Hidden -File "C:\NextApp\1.ps1" -exit
+
+powershell -ExecutionPolicy Bypass -WindowStyle Hidden -File "%~dp01.ps1" -exit
