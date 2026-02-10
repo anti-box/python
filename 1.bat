ef36e85a2ad233d191ecd84d0091be6ab4b54327
anti-box <bclee6c@daum.net>
2026-02-10 15:12:23 +0700

    Update 1.bat

--- 1.bat
+++ 1.bat
@@ -1,4 +1,5 @@
 mode 15,1
 @echo off
-powershell -ExecutionPolicy Bypass -WindowStyle Hidden -File "C:/mng-kybooksch/1.ps1" -exit
+powershell -ExecutionPolicy Bypass -WindowStyle Hidden -File "C:\mng-kybooksch\1.ps1" -exit
+
 
